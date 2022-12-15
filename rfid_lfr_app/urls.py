476602e5a9220dfc1a1e40303f0d76163f0420e7
IftekharulAlam <iftekharulalam38@gmail.com>
2022-12-15 22:05:35 +0600

    added

--- rfid_lfr_app/urls.py
+++ rfid_lfr_app/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
 
+    path('controlCar', views.controlCar, name='controlCar'),
     path('writelocation', views.writelocation, name='writelocation'),
-    path('readlocation', views.readlocation, name='readlocation'),
+    path('readStatus', views.readStatus, name='readStatus'),
+
+
 
 
 ]
--- rfid_lfr_app/views.py
+++ rfid_lfr_app/views.py
@@ -7,25 +7,54 @@ import json
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 
-
+@csrf_exempt
 def writelocation(request):
     if request.method == 'GET':
-        Location = request.GET.get("Location", False)
+        botNumber = "1"
+        Locationo = request.GET.get("Locationo", False)
+        Status = request.GET.get("Status", False)
         with connection.cursor() as cursor_1:
             cursor_1.execute(
-                "INSERT INTO bot_location(Location) VALUES ('"+str(Location) + "' )")
-    return HttpResponse("Hello, world. You're at the polls index.")
+                "INSERT INTO bot_location(botNumber, Location, Status) VALUES ('"+str(botNumber) + "','" + str(Locationo) + "','" + str(Status) + "' )")
+            connection.close()
+        return HttpResponse()
 
-def readlocation(request):
+@csrf_exempt
+def controlCar(request):
+    if request.method == 'POST':
+        with connection.cursor() as cursor_1:
+            cursor_1.execute("SELECT * from bot_location")
+            row1 = cursor_1.fetchone()
+        if row1 == None:
+            pass
+        else:
+            status = request.POST.get("status", False)
+            botNumber = "1"
+            if status == "Start":
+                with connection.cursor() as cursor_1:
+                    cursor_1.execute(
+                        "update bot_location set Status='" + str(status) + "' where botNumber = '"+ str(botNumber) + "'")
+                    connection.close()
+            else:
+                with connection.cursor() as cursor_1:
+                    cursor_1.execute(
+                        "update bot_location set Status='" + str(status) + "' where botNumber = '"+ str(botNumber) + "'")
+                    connection.close()
+    return HttpResponse()
+
+@csrf_exempt
+def readStatus(request):
     if request.method == 'GET':
         with connection.cursor() as cursor_1:
-            cursor_1.execute("select Serial, Location from bot_location")
-            row1 = cursor_1.fetchall()
-            result = []
-            keys = ('Serial', 'Location')
-            for row in row1:
-                result.append(dict(zip(keys, row)))
-            json_data = json.dumps(result)
-            return HttpResponse(json_data, content_type="application/json")
-
-
+            cursor_1.execute("SELECT Status from bot_location order by Serial DESC LIMIT 1")
+            row1 = cursor_1.fetchone()
+            connection.close()
+            if row1 == None:
+                return HttpResponse("", content_type="application/json")
+            else:
+                response_data = {}
+                response_data['result'] = row1[0]
+                json_data = json.dumps(response_data)
+                return HttpResponse(json_data, content_type="application/json")
+    return HttpResponse()
+        
